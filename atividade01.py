a2cdc024b97a2d203c472c9371641cca71add266
Arthurgon03 <tad-arthursilva@ucpparana.edu.br>
2023-08-11 19:54:43 -0300

    Atividade 01

---
+++ atividade01.py
@@ -0,0 +1,14 @@
+nome = ('Arthur')
+sobrenome = ('Gonçalves da Silva')
+idade = ('19')
+altura = ('1.78')
+peso = ('97Kg')
+
+print(nome, sobrenome)
+print(idade)
+print(altura)
+print(peso)
+if idade > '18':
+    print('Maior de idade')
+else:
+    print('Menor de idade')
